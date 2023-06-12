53fe6151e4cbb3a9df4b1c5bf1157be71ca84b39
tuankiet116 <kietdat1612000@gmail.com>
2023-06-13 00:40:06 +0700

    Problems_Solving: recursive, linked list

--- Problems_Solving/AddTwoNumber.js
+++ Problems_Solving/AddTwoNumber.js
@@ -9,29 +9,25 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    let maxLength = Math.max(l1.length, l2.length);
-    let result = [];
-    let listnode = ListNode();
-    let remember = 0;
-    for (let i = 0; i < maxLength; ++i) {
-        let sum = (l1[i] ?? 0 + l2[i] ?? 0 + remember).split('');
-        if (sum.length > 1) {
-            remember = sum[0];
-            result.push(sum[1]);
-        } else {
-            remember = 0;
-            result.push(sum[0]);
-        }
+    let value1 = l1?.val ?? 0;
+    let value2 = l2?.val ?? 0;
+    let sum = (value1 + value2).toString().split('');
+    let rememberNum = sum.length > 1 ? parseInt(sum[0]) : 0;
+    if (l1.next || l2.next) {
+        let holdRemember = l1.next ? l1 : l2;
+        holdRemember.next.val += rememberNum;
+        return new ListNode(sum[sum.length - 1], addTwoNumbers(l1?.next, l2?.next));
     }
-    return l1.length;
+    return new ListNode(parseInt(sum[sum.length - 1]) + rememberNum)
 };
-let l3 = new ListNode(3);
-let l2 = new ListNode(4, l3);
-let l1 = new ListNode(2, l2);
+let l3 = new ListNode(4);
+let l2 = new ListNode(6, l3);
+let l1 = new ListNode(5, l2);
 
 
 let n3 = new ListNode(3);
 let n2 = new ListNode(4, n3);
 let n1 = new ListNode(2, n2);
 
+// console.log(n1.next);
 console.log(addTwoNumbers(n1, l1));
